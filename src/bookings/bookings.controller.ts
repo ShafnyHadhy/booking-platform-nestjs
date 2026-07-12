07801bda2518593cca09ecab1530ec824c03a225
Shafny Hadhy <mohamedshafny02@gmail.com>
2026-07-12 21:05:42 +0530

    implement findAll and findOne methods in BookingsController with JWT authentication

--- src/bookings/bookings.controller.ts
+++ src/bookings/bookings.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, UseGuards } from '@nestjs/common';
 import { BookingsService } from './bookings.service';
 import { CreateBookingDto } from './dto/create-booking.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { BookingStatus } from '@prisma/client';
 
 @Controller('bookings')
 export class BookingsController {
@@ -11,4 +13,17 @@ export class BookingsController {
     create(@Body() createBookingDto: CreateBookingDto) {
         return this.bookingsService.create(createBookingDto);
     }
+
+    @UseGuards(JwtAuthGuard)
+    @Get()
+    findAll(@Query('status') status?: BookingStatus) {
+        return this.bookingsService.findAll(status);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.bookingsService.findOne(id);
+    }
+
 }
